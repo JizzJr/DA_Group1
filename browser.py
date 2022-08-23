d30f309872bc04f5a85c5615f27b8caaa922acab
JizzJR <jovantanhao@gmail.com>
2022-08-23 16:42:54 +0800

    v1.0

---
+++ browser.py
@@ -0,0 +1,27 @@
+# Use the Request library
+import requests
+# Set the target webpage
+url = "http://172.18.58.80/headers.php" #(put sch url)
+r = requests.get(url)
+
+# This will get the full page
+print(r.text)
+
+print("Status code:")
+print("\t *", r.status_code)
+h = requests.head(url)
+
+print("Header:")
+print("**********")
+# To print line by line
+for x in h.headers:
+    print("\t ", x, ":", h.headers[x])
+print("**********")
+headers = {
+    'User-Agent' : "Mobile"
+}
+# Test it on an external site
+url2 = 'http://httpbin.org/headers'
+rh = requests.get(url2, headers=headers)
+print(rh.text)
+
